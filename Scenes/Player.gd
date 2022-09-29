a52a1e207d4f6cbfa54c9181d960b9e0dafc52f7
ikenton <isabellakenton@gmail.com>
2022-09-29 11:16:30 -0700

    removed fast fell

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -5,7 +5,7 @@ export(int) var gravity = 4
 export(int) var jump_speed = -200
 export(int) var jump_release = -50
 export(int) var friction = 20
-export(int) var fell = 25
+export(int) var additional_gravity = 10
 var fast_fell = false 
 
 
@@ -19,21 +19,21 @@ func _physics_process(delta):
 		apply_acceleration(input.x)
 	
 	if is_on_floor():
-		fast_fell = false
 		if Input.is_action_pressed("jump"): # can hold down jump button to keep jumping
 			velocity.y = jump_speed #jump height
 	else:
 		if Input.is_action_just_released("jump") and velocity.y < jump_release:
 			velocity.y = jump_release #jump release height, longer the jump button is pressed higher the jump
 		
-		if velocity.y > 10 and not fast_fell:
-			velocity.y += fell
-			fast_fell = true
+		if velocity.y > 0:
+			velocity.y += additional_gravity
+			
 	
 	velocity = move_and_slide(velocity, Vector2.UP) #move character and detect/handle collisions
 
 func apply_gravity():
 	velocity.y += gravity
+	velocity.y = min(velocity.y, 1000)
 
 func apply_friction():
 	velocity.x = move_toward(velocity.x, 0, friction)
