1df04780f483689efe9955eb14856bb4b0e8a12f
ikenton <isabellakenton@gmail.com>
2022-09-29 18:04:43 -0700

    enemy no longer clips edge

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -3,11 +3,12 @@ extends KinematicBody2D
 var direction = Vector2.RIGHT
 var velocity = Vector2.ZERO
 
-onready var ledgeCheck: = $LedgeCheck
+onready var ledgeCheckRight: = $LedgeCheckRight
+onready var ledgeCheckLeft = $LedgeCheckLeft
 
 func _physics_process(delta):
 	var found_wall = is_on_wall() # detects bumping into a wall
-	var found_ledge = not ledgeCheck.is_colliding()
+	var found_ledge = not ledgeCheckRight.is_colliding() or not ledgeCheckLeft.is_colliding()
 	if found_wall or found_ledge:
 		direction *= -1
 	velocity = direction * 25
